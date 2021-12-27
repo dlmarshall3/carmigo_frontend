e017c51214f029e3bd95870bedd5a0ff00bc3ad7
dlmarshall3 <dlmarshall3@protonmail.com>
2021-12-27 15:09:44 -0600

    moved files to root

---
+++ src/components/Body.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="body">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'body',
+  components: {
+      
+  }
+}
+</script>
+
+<style>
+#body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+}
+</style>
